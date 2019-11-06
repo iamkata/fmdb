ba773c5f2c9692f0a9b9206c533466801c9936f1
iamkata <35477358+iamkata@users.noreply.github.com>
2019-11-06 17:44:29 +0800

    Add files via upload

---
+++ FMDB的使用/FMDB的使用/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FMDB的使用
+//
+//  Created by 徐金城 on 2019/11/6.
+//  Copyright © 2019 xujincheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FMDB的使用/FMDB的使用/ViewController.m
@@ -0,0 +1,89 @@
+//
+//  ViewController.m
+//  FMDB的使用
+//
+//  Created by 徐金城 on 2019/11/6.
+//  Copyright © 2019 xujincheng. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "FMDB.h"
+
+@interface ViewController ()
+
+@property(nonatomic,strong)FMDatabase *db;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+     //1.获得数据库文件的路径
+     NSString *doc=[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+     NSString *fileName=[doc stringByAppendingPathComponent:@"student.sqlite"];
+
+    NSLog(@"%@",fileName);
+     //2.获得数据库
+     FMDatabase *db=[FMDatabase databaseWithPath:fileName];
+
+     //3.打开数据库
+     if ([db open]) {
+         //4.创表
+         BOOL result=[db executeUpdate:@"CREATE TABLE IF NOT EXISTS t_student (id integer PRIMARY KEY AUTOINCREMENT, name text NOT NULL, age integer NOT NULL);"];
+         if (result) {
+             NSLog(@"创表成功");
+         }else {
+             NSLog(@"创表失败");
+         }
+     }
+     self.db=db;
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    //[self insert];
+    //[self query];
+    [self delete];
+}
+
+//插入数据
+-(void)insert {
+    for (int i = 0; i<10; i++) {
+        NSString *name = [NSString stringWithFormat:@"jack-%d", arc4random_uniform(100)];
+        // executeUpdate : 不确定的参数用?来占位
+        BOOL isSucess = [self.db executeUpdate:@"INSERT INTO t_student (name, age) VALUES (?, ?);", name, @(arc4random_uniform(40))];
+        if (isSucess) {
+            NSLog(@"插入成功");
+        } else {
+            NSLog(@"插入失败");
+        }
+        //        [self.db executeUpdate:@"INSERT INTO t_student (name, age) VALUES (?, ?);" withArgumentsInArray:@[name, @(arc4random_uniform(40))]];
+
+         // executeUpdateWithFormat : 不确定的参数用%@、%d等来占位
+         //        [self.db executeUpdateWithFormat:@"INSERT INTO t_student (name, age) VALUES (%@, %d);", name, arc4random_uniform(40)];
+    }
+}
+
+//删除数据
+-(void)delete {
+    //    [self.db executeUpdate:@"DELETE FROM t_student;"];
+    [self.db executeUpdate:@"DROP TABLE IF EXISTS t_student;"];
+    //[self.db executeUpdate:@"CREATE TABLE IF NOT EXISTS t_student (id integer PRIMARY KEY AUTOINCREMENT, name text NOT NULL, age integer NOT NULL);"];
+}
+
+//查询
+- (void)query {
+    // 1.执行查询语句
+    FMResultSet *resultSet = [self.db executeQuery:@"SELECT * FROM t_student"];
+
+    // 2.遍历结果
+    while ([resultSet next]) {
+        int ID = [resultSet intForColumn:@"id"];
+        NSString *name = [resultSet stringForColumn:@"name"];
+        int age = [resultSet intForColumn:@"age"];
+        NSLog(@"%d %@ %d", ID, name, age);
+    }
+}
+
+@end
